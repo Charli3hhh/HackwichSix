e365214b5803cea6cda62b4f54c784b81b1447f6
Charles Ombao <combao@hawaii.edu>
2020-03-03 10:33:01 -1000

    Completed Parts 1-9 HackwichSix, Must complete Problem Set 1

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var myFriendsArray = ["Vien","Michael B.", "Mason"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        //Dispose of any resources that can be recreated
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")
+        let text = myFriendsArray[indexPath.row]
+        cell?.textLabel?.text = text
+        return cell!
+    }
+ 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myFriendsArray.count
+    }
 
 }
 
